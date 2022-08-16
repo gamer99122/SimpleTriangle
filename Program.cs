4c50e9f3f9a7f87fcadbe9c9b9ab0920b8862f73
S.F <gamer99122@hotmail.com.tw>
2022-08-16 21:01:03 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            初階三角形();  
+        }
+        public static void 初階三角形()
+        {
+            Console.WriteLine("初階三角形:");
+
+
+            //第一列的空白
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write(" ");
+            }
+
+            //第一列的星星*
+            for (int i = 2; i < 3; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();//換行
+
+            //第二列的空白
+            for (int i = 0; i < 1; i++)
+            {
+                Console.Write(" ");
+            }
+
+            //第二列的星星*
+            for (int i = 1; i < 4; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();//換行
+
+            //第三列星星不需要空白
+
+
+            //第三列的星星*
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("*");
+            }
+
+            //Console.Read()，可以讓主控台不會因為執行完(↑上述程式碼)就直接關閉
+            Console.Read();
+        }
+
+    }
+}

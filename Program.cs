48d307b733870745e8bb3d90efc1762cf20b35a9
S.F <gamer99122@hotmail.com.tw>
2022-08-16 21:36:44 +0800

    1.新增，初階三角形2 2.調整變數值

--- Program.cs
+++ Program.cs
@@ -10,21 +10,91 @@ namespace SimpleTriangle
     {
         static void Main(string[] args)
         {
-            初階三角形();  
+            初階三角形1();
+            初階三角形2();
+
+
+
+
+            //Console.Read()，可以讓主控台不會因為執行完(↑上述程式碼)就直接關閉
+            Console.Read();
         }
-        public static void 初階三角形()
+
+
+        public static void 初階三角形2()
+        {
+            Console.WriteLine("初階三角形1:");
+            Console.WriteLine("-------------------");
+
+            for (int i = 1; i < 4; i++)
+            {
+                //第一列
+                if (i == 1)
+                {
+                    //空白
+                    for (int j = 1; j < 3; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    //星星*
+                    for (int j = 3; j < 4; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+
+                //第二列
+                if (i == 2)
+                {
+                    //空白
+                    for (int j = 1; j < 2; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    //星星*
+                    for (int j = 2; j < 5; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+                //第三列
+                if (i == 3)
+                {
+                    //空白
+                    //--------
+
+                    //星星*
+                    for (int j = 1; j < 6; j++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+
+        public static void 初階三角形1()
         {
             Console.WriteLine("初階三角形:");
-
+            Console.WriteLine("-------------------");
 
             //第一列的空白
-            for (int i = 0; i < 2; i++)
+            for (int i = 1; i < 3; i++)
             {
                 Console.Write(" ");
             }
 
             //第一列的星星*
-            for (int i = 2; i < 3; i++)
+            for (int i = 3; i < 4; i++)
             {
                 Console.Write("*");
             }
@@ -32,13 +102,13 @@ namespace SimpleTriangle
             Console.WriteLine();//換行
 
             //第二列的空白
-            for (int i = 0; i < 1; i++)
+            for (int i = 1; i < 2; i++)
             {
                 Console.Write(" ");
             }
 
             //第二列的星星*
-            for (int i = 1; i < 4; i++)
+            for (int i = 2; i < 5; i++)
             {
                 Console.Write("*");
             }
@@ -49,13 +119,13 @@ namespace SimpleTriangle
 
 
             //第三列的星星*
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i < 6; i++)
             {
                 Console.Write("*");
             }
 
-            //Console.Read()，可以讓主控台不會因為執行完(↑上述程式碼)就直接關閉
-            Console.Read();
+            Console.WriteLine();
+            Console.WriteLine();
         }
 
     }
